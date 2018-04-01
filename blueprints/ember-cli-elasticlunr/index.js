d47bcb24258d5917e246e45f0688c5e8f3ac17b9
Eguzki <eguzki@lacueva.io>
2018-04-01 00:33:39 -0600

    added normalizeEntityName to blueprint

--- blueprints/ember-cli-elasticlunr/index.js
+++ blueprints/ember-cli-elasticlunr/index.js
@@ -1,6 +1,12 @@
 /* eslint-env node */
 module.exports = {
-  description: 'Generates an empty instance initializer with instructions for creating elasticlunr indexes'
+  description: 'Generates an empty instance initializer with instructions for creating elasticlunr indexes',
+  
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   // locals(options) {
   //   // Return custom template variables here.
