11c9d6e73bd3d37ac0eb9114dfaa7314de6872eb
Eguzki <eguzki@lacueva.io>
2018-04-01 00:14:52 -0600

    added initializer blueprint

---
+++ blueprints/ember-cli-elasticlunr/files/app/instance-initializers/initialize-elasticlunr-indexes.js
@@ -0,0 +1,24 @@
+/* global elasticlunr */
+
+	
+/*
+	Use this file to initialize elasticlunr indexes, it can be done like in the example below.
+	For more info on elastic lunr options, take a look at the docs here -> http://elasticlunr.com/docs/index.html
+ */
+ 
+// function itemIndexData () {
+// 	this.addField('name');
+// 	this.setRef('id');
+// 	this.pipeline.remove(elasticlunr.stopWordFilter);
+// 	this.pipeline.remove(elasticlunr.stemmer);
+// }
+
+export function initialize( /*appInstance*/ ) {
+  // let elasticlunr = appInstance.lookup('service:elasticlunr');
+  // elasticlunr.createIndex('item', itemIndexData);
+}
+
+
+export default {
+  initialize
+};
---
+++ blueprints/ember-cli-elasticlunr/index.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  description: 'Generates an empty instance initializer with instructions for creating elasticlunr indexes'
+
+  // locals(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall(options) {
+  //   // Perform extra work here.
+  // }
+};
